41062d93e6c1f746ac06366c8106ed7249a96bb8
Billijk <wang-zy14@mails.tsinghua.edu.cn>
2016-05-10 10:28:30 +0800

    项目初始化

---
+++ ping-pong/pingpong.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
+use ieee.std_logic_arith.all;
+
+entity pingpong is
+port();
+end entity;
+
+architecture behav of pingpong is
+begin
+end architecture;
